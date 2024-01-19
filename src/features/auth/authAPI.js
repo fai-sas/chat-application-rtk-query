7a75ed3b7d5f5d723ff20b9eec2903d6a82d79d7
fai-sas <craftedbyfaisal@gmail.com>
2024-01-19 22:07:21 +0600

    auth api and slice created

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,7 +1,65 @@
 /* eslint-disable no-unused-vars */
 
 import { apiSlice } from '../api/apiSlice'
+import { userLoggedIn } from './authSlice'
 
 export const authAPI = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: '/register',
+        method: 'POST',
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled
+          localStorage.setItem(
+            'auth',
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+        } catch (error) {
+          console.error(error)
+        }
+      },
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: '/login',
+        method: 'POST',
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled
+          localStorage.setItem(
+            'auth',
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+        } catch (error) {
+          console.error(error)
+        }
+      },
+    }),
+  }),
 })
+
+export const { useRegisterMutation, useLoginMutation } = authAPI
