0e3a0ddf5457f7e29a09ad8c12511593072d79de
fai-sas <craftedbyfaisal@gmail.com>
2024-01-24 20:11:04 +0600

    messages api created

--- src/features/messages/messagesAPI.js
+++ src/features/messages/messagesAPI.js
@@ -3,5 +3,14 @@
 import { apiSlice } from '../api/apiSlice'
 
 export const messagesAPI = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    getMessages: builder.query({
+      query: (id) =>
+        `/conversations?conversationId=${id}&_sort=timestamp&_order=desc&page=1&_limit=${
+          import.meta.env.VITE_MESSAGES_PER_PAGE
+        }`,
+    }),
+  }),
 })
+
+export const { useGetMessagesQuery } = messagesAPI
